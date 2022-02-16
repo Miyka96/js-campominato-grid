f81183019759edf8a0c4adde033bfefb48f0f7b8
Miyka96 <micaelaseminara@outlook.it>
2022-02-16 20:11:38 +0100

    progress

--- app.js
+++ app.js
@@ -2,42 +2,51 @@ const btn = document.getElementById('difficulty-btn');
 
 
 
-btn.addEventListener('click',function(){
+btn.addEventListener('click', function () {
 
     const grid = document.querySelector(".grid");
 
-    let difficulty= document.getElementById('difficulty').value;
+    let difficulty = document.getElementById('difficulty').value;
 
-    let cell
-    let columns
-    
+    let cell = 0;
+    let columns =0;
+    let cellSize
+
+
     switch (difficulty) {
-        case "1" :
+        case "1":
             cell = 100;
             columns = 10;
-        break;
-    
-        case "2" :
-            cell= 81;
+            console.log(cell, columns)
+            break;
+
+        case "2":
+            cell = 81;
             columns = 9;
-        break;
-    
-        case "3" :
-            cell= 49;
-            columns= 7;
-        break;
-    
-        default :
-            console.log('1')
+            console.log(cell, columns)
+            break;
+
+        case "3":
+            cell = 49;
+            columns = 7;
+            console.log(cell, columns)
+            break;
+
+        default:
+            cell = 100;
+            columns = 10;
+            console.log(cell, columns)
     }
 
-    for( let i=1; i<cell+1; i++ ){
-        let cellElement = document.createElement('div');
-        grid.append(cellElement);
-        cellElement.classList.add('cell');
-        cellElement.style.width= `calc( 400px / ${columns})`;
-        cellElement.style.height= `30px`;
-    }
+for (let i = 1; i < cell + 1; i++) {
+    let cellElement = document.createElement('div');
+    grid.append(cellElement);
+    cellElement.append(i);
+    cellElement.classList.add('cell');
+
+    cellSize = `calc(100% / ${columns})`
+    cellElement.style.width = cellSize;
+}
 })
 
 
